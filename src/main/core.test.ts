d7516c88daad4afed73bd682a70d2d2c562c4390
Oliver Schwendener <oliver.schwendener@hotmail.com>
2021-04-05 18:41:22 +0200

    Removed unused core files

--- src/main/core.test.ts
+++
@@ -1,8 +0,0 @@
-import { add } from "./core";
-
-describe(add.name, () => {
-    it("should add two numbers", () => {
-        const actual = add(1, 2);
-        expect(actual).toBe(3);
-    });
-});
