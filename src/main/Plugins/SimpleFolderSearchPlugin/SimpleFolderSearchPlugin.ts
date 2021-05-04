2f4b0fc11dfebda128d32a68ba10ff28b2d25e99
Oliver Schwendener <oliver.schwendener@hotmail.com>
2021-05-04 23:34:26 +0200

    Improved SimpleFolderSearchPlugin

--- src/main/Plugins/SimpleFolderSearchPlugin/SimpleFolderSearchPlugin.ts
+++ src/main/Plugins/SimpleFolderSearchPlugin/SimpleFolderSearchPlugin.ts
@@ -1,21 +1,38 @@
 import { ApplicationRuntimeInformation } from "../../ApplicationRuntimeInformation";
+import { FileIconUtility } from "../../Utilities/FileIconUtility";
+import { FileSystemUtility } from "../../Utilities/FileSystemUtility";
 import { SearchPlugin } from "../SearchPlugin";
 import { SimpleFolderSearchResultItem } from "./SimpleFolderSearchResultItem";
 
 export class SimpleFolderSearchPlugin extends SearchPlugin {
+    public readonly pluginId = "SimpleFolderSearchPlugin";
+
+    private files: { filePath: string; icon: string }[];
+
     public constructor(applicationRuntimeInformation: ApplicationRuntimeInformation) {
-        super("SimpleFolderSearchPlugin", applicationRuntimeInformation);
+        super(applicationRuntimeInformation);
+        this.files = [];
     }
 
     public clearCache(): Promise<void> {
         return Promise.resolve();
     }
 
-    public rescan(): Promise<void> {
-        return Promise.resolve();
+    public async rescan(): Promise<void> {
+        const filePaths = await FileSystemUtility.getFolderItems(this.applicationRuntimeInformation.userHomePath);
+        this.files = await Promise.all(filePaths.map((filePath) => this.getIcon(filePath)));
     }
 
     public getAllItems(): SimpleFolderSearchResultItem[] {
-        return [];
+        return this.files.map(
+            (file): SimpleFolderSearchResultItem => new SimpleFolderSearchResultItem(file.filePath, file.icon)
+        );
+    }
+
+    private async getIcon(filePath: string): Promise<{ filePath: string; icon: string }> {
+        return {
+            filePath,
+            icon: await FileIconUtility.getIconDataUrlFromFilePath(filePath),
+        };
     }
 }
--- src/main/Plugins/SimpleFolderSearchPlugin/SimpleFolderSearchResultItem.ts
+++ src/main/Plugins/SimpleFolderSearchPlugin/SimpleFolderSearchResultItem.ts
@@ -1,16 +1,21 @@
+import { basename } from "path";
 import { SearchResultItem } from "../../../common/SearchResultItem";
 import { Searchable } from "../../Core/Searchable";
+import { FilePathExecutor } from "../../Executors/FilePathExecutor";
+import { FilePathLocationOpener } from "../../LocationOpeners/FilePathLocationOpener";
 
 export class SimpleFolderSearchResultItem implements Searchable {
+    public constructor(private readonly filePath: string, private readonly icon: string) {}
+
     public toSearchResultItem(): SearchResultItem {
         return {
-            description: "",
-            executionArgument: "",
-            executorId: "",
-            icon: "",
-            locationOpenerId: "",
-            name: "",
-            openLocationArgument: "",
+            description: this.filePath,
+            executionArgument: this.filePath,
+            executorId: FilePathExecutor.executorId,
+            icon: this.icon,
+            locationOpenerId: FilePathLocationOpener.locationOpenerId,
+            name: basename(this.filePath),
+            openLocationArgument: this.filePath,
         };
     }
 }
