5152f5a7402a1539f80794ecdd39899acc765519
Oliver Schwendener <oliver.schwendener@hotmail.com>
2021-05-04 23:33:13 +0200

    Improved readability of WindowsApplicationSearchPlugin by extracting powershell scripts to class constants

--- src/main/Plugins/WindowsApplicationSearchPlugin/WindowsApplicationSearchPlugin.ts
+++ src/main/Plugins/WindowsApplicationSearchPlugin/WindowsApplicationSearchPlugin.ts
@@ -5,6 +5,71 @@ import { SearchPlugin } from "../SearchPlugin";
 import { ApplicationRuntimeInformation } from "../../ApplicationRuntimeInformation";
 
 export class WindowsApplicationSearchPlugin extends SearchPlugin {
+    private static readonly extractShortcutPowershellScript = `
+        function Extract-Shortcut {
+            param(
+                [string]$ShortcutFilePath
+            )
+
+            try {
+                $Shell = New-Object -ComObject WScript.Shell
+                $TargetPath = $Shell.CreateShortcut($ShortcutFilePath).TargetPath
+                $TargetPathAccessible = Test-Path -Path $TargetPath -PathType Leaf
+                if ($TargetPathAccessible) {
+                    return $TargetPath;
+                }
+                else {
+                    return $ShortcutFilePath
+                }
+            }
+            catch {
+                return $ShortcutFilePath
+            }
+        }
+    `;
+
+    private static readonly getWindowsAppsPowershellScript = `
+        function Get-WindowsApps {
+            param(
+                [string[]]$FolderPaths,
+                [string[]]$FileExtensions,
+                [string]$AppIconFolder
+            )
+
+            Add-Type -AssemblyName System.Drawing
+
+            $Utf8 = New-Object -TypeName System.Text.UTF8Encoding
+
+            $Files = Get-ChildItem -File -Path $FolderPaths -Recurse -Include $FileExtensions | Select-Object -Property Name, FullName, Extension, BaseName
+
+            foreach ($File in $Files) {
+                $Stream = [System.IO.MemoryStream]::new($Utf8.GetBytes($File.FullName))
+                $Hash = Get-FileHash -Algorithm MD5 -InputStream $Stream | Select-Object -ExpandProperty Hash
+                $IconFilePath = "$($AppIconFolder)\\$($Hash).png"
+                $File | Add-Member -MemberType NoteProperty -Name "IconFilePath" -Value $IconFilePath
+
+                $IconAlreadyExists = Test-Path -LiteralPath $File.IconFilePath
+
+                if (!$IconAlreadyExists) {
+                    $FilePathToExtractIcon = $File.FullName
+
+                    if ($File.Extension -eq ".lnk") {
+                        $FilePathToExtractIcon = Extract-Shortcut -ShortcutFilePath $File.FullName
+                    }
+
+                    $Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($FilePathToExtractIcon);
+                    if ($Icon -ne $null) {
+                        $Icon.ToBitmap().Save($File.IconFilePath, [System.Drawing.Imaging.ImageFormat]::Png)
+                    }
+                }
+            }
+
+            $Files | ConvertTo-Json -Compress
+        }
+    `;
+
+    public readonly pluginId = "WindowsApplicationSearchPlugin";
+
     private applications: WindowsApplication[];
 
     public constructor(
@@ -12,7 +77,7 @@ export class WindowsApplicationSearchPlugin extends SearchPlugin {
         private readonly executePowershellScript: (powershellScript: string) => Promise<string>,
         private readonly applicationSearchPreferences: WindowsApplicationSearchPreferences
     ) {
-        super("WindowsApplicationSearchPlugin", applicationRuntimeInformation);
+        super(applicationRuntimeInformation);
         this.applications = [];
     }
 
@@ -36,64 +101,8 @@ export class WindowsApplicationSearchPlugin extends SearchPlugin {
 
     private getPowershellScript(): string {
         return `
-            function Extract-Shortcut {
-                param(
-                    [string]$ShortcutFilePath
-                )
-            
-                try {
-                    $Shell = New-Object -ComObject WScript.Shell
-                    $TargetPath = $Shell.CreateShortcut($ShortcutFilePath).TargetPath
-                    $TargetPathAccessible = Test-Path -Path $TargetPath -PathType Leaf
-                    if ($TargetPathAccessible) {
-                        return $TargetPath;
-                    }
-                    else {
-                        return $ShortcutFilePath
-                    }
-                }
-                catch {
-                    return $ShortcutFilePath
-                }
-            }
-
-            function Get-WindowsApps {
-                param(
-                    [string[]]$FolderPaths,
-                    [string[]]$FileExtensions,
-                    [string]$AppIconFolder
-                )
-            
-                Add-Type -AssemblyName System.Drawing
-            
-                $Utf8 = New-Object -TypeName System.Text.UTF8Encoding
-            
-                $Files = Get-ChildItem -File -Path $FolderPaths -Recurse -Include $FileExtensions | Select-Object -Property Name, FullName, Extension, BaseName
-            
-                foreach ($File in $Files) {
-                    $Stream = [System.IO.MemoryStream]::new($Utf8.GetBytes($File.FullName))
-                    $Hash = Get-FileHash -Algorithm MD5 -InputStream $Stream | Select-Object -ExpandProperty Hash
-                    $IconFilePath = "$($AppIconFolder)\\$($Hash).png"
-                    $File | Add-Member -MemberType NoteProperty -Name "IconFilePath" -Value $IconFilePath
-            
-                    $IconAlreadyExists = Test-Path -LiteralPath $File.IconFilePath
-            
-                    if (!$IconAlreadyExists) {
-                        $FilePathToExtractIcon = $File.FullName
-            
-                        if ($File.Extension -eq ".lnk") {
-                            $FilePathToExtractIcon = Extract-Shortcut -ShortcutFilePath $File.FullName
-                        }
-            
-                        $Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($FilePathToExtractIcon);
-                        if ($Icon -ne $null) {
-                            $Icon.ToBitmap().Save($File.IconFilePath, [System.Drawing.Imaging.ImageFormat]::Png)
-                        }
-                    }
-                }
-            
-                $Files | ConvertTo-Json -Compress
-            }
+            ${WindowsApplicationSearchPlugin.extractShortcutPowershellScript}
+            ${WindowsApplicationSearchPlugin.getWindowsAppsPowershellScript}
 
             Get-WindowsApps -FolderPaths ${this.getFolderPathFilter()} -FileExtensions ${this.getFileExtensionFilter()} -AppIconFolder ${this.getTemporaryFolderPath()}
         `;
